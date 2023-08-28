831a2e15a135905de9af77e24c389b7b21d188ac
Rodrigo Villca <villcarodrigo70@gmail.com>
2023-08-28 14:06:01 -0300

    agregado de usuarios y posts

---
+++ modificacion/inserts-usuario_post.sql
@@ -0,0 +1,13 @@
+INSERT INTO user(username,password)
+VALUES("Rodrigo", "1234");
+INSERT INTO user(username,password)
+VALUES ("Ivan", "4321");
+
+INSERT INTO USER (username, password)
+VALUES ("Nahuel","45678");
+
+
+INSERT into post(author_id,title,body)
+VALUES("1","saludar","hola mundo"),
+("2","saluda al mundo","hola mundo"),
+("3","salud mundo","hola mundo");
